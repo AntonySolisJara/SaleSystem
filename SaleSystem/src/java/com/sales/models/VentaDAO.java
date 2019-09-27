924c5f0022fde069032f73d02d962feed95934ef
Antony <antonysolisjara@gmail.com>
2019-09-26 19:12:15 -0600

    Avance 3 guardar venta

--- SaleSystem/src/java/com/sales/models/VentaDAO.java
+++ SaleSystem/src/java/com/sales/models/VentaDAO.java
@@ -1,4 +1,9 @@
 package com.sales.models;
+import com.sales.config.Conexion;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 /**
  *
@@ -6,4 +11,80 @@ package com.sales.models;
  */
 public class VentaDAO {
     
+    Connection con;
+    Conexion cn;
+    PreparedStatement ps;
+    ResultSet rs;
+    int r;
+    
+    public String generarSerie(){
+        String numeroSerie = "";
+        String sql = "select max(NumeroSerie) from ventas";
+        try{
+            con = cn.Conexion();
+            ps = con.prepareStatement(sql);
+            rs = ps.executeQuery();
+            while(rs.next()){
+                numeroSerie = rs.getString(1);
+            }
+        }
+        catch(SQLException e){
+            
+        }
+        return numeroSerie;
+    }
+    
+    public String idVenta() {
+        String idVenta = "";
+        String sql = "select max(IdVentas) from ventas";
+        try{
+            con = cn.Conexion();
+            ps = con.prepareStatement(sql);
+            rs = ps.executeQuery();
+            while(rs.next()){
+                idVenta = rs.getString(1);
+            }
+        }
+        catch(SQLException e){
+            
+        }
+        return idVenta;
+    }
+
+    public int guardarVenta(VentaModel ve) {
+        String sql = "insert into ventas(IdCliente, IdEmpleado, NumeroSerie, FechaVentas, Monto, Estado) values(?, ?, ?, ?, ?, ?)";
+        try{
+            con = cn.Conexion();
+            ps = con.prepareStatement(sql);
+            ps.setInt(1, ve.getIdCliente());
+            ps.setInt(2, ve.getIdEmpleado());
+            ps.setString(3, ve.getSerie());
+            ps.setString(4, ve.getFecha());
+            ps.setDouble(5, ve.getMonto());
+            ps.setString(6, ve.getEstado());
+            ps.executeUpdate();
+        }
+        catch(SQLException e){
+            
+        }
+        return r;
+    
+    }
+
+    public int guardarDetalleVenta(VentaModel ve) {
+        String sql = "insert into detalle_ventas(IdVentas, IdProducto, Cantidad, PrecioVenta) values(?, ?, ?, ?)";
+        try{
+            con = cn.Conexion();
+            ps = con.prepareStatement(sql);
+            ps.setInt(1, ve.getId());
+            ps.setInt(2, ve.getIdProducto());
+            ps.setInt(3, ve.getCantidad());
+            ps.setDouble(4, ve.getPrecio());
+            ps.executeUpdate();
+        }
+        catch(SQLException e){
+            
+        }
+        return r;
+    }
 }
